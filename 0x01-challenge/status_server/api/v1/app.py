5e9ea974d3bd0303dc7094f6c809870f440d2061
maroua199525 <2646@holbertonschool.com>
2021-10-01 21:11:58 +0100

    fix task

--- 0x01-challenge/status_server/api/v1/app.py
+++ 0x01-challenge/status_server/api/v1/app.py
@@ -3,7 +3,7 @@
 Web server
 """
 from api.v1.views import app_views
-from flask import Flask, make_response, jsonify
+from flask import Flask,  jsonify, make_response
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -18,4 +18,4 @@ def not_found(error):
 
 if __name__ == "__main__":
     # python3 -m api.v1.app
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
