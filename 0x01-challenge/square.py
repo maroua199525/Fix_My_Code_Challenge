f1ea2a7b77d5f7b028c4d3a55b0d9c2d85c9cf53
maroua199525 <2646@holbertonschool.com>
2021-10-01 14:20:14 +0100

    fix code

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
 
 class square():
-    
-    width = 0
-    height = 0
-
-    
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-    def area_of_my_square(self):
+    def area_of_my_square(self, width = 0, height = 0):
         """ Area of the square """
         return (self.width * self.height)
 
